0eac92c6f14365c24578892a50e0034444910637
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-03-21 00:35:11 +0100

    Got CSS in place

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from '../logo.svg';
-import '../App.css';
 
 
 function Courses() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Learn React
-                </a>
+        <React.Fragment>
+            <header>
+                <div className="wrap header--flex">
+                    <h1 className="header--logo"><a href="index.html">Courses</a></h1>
+                    <nav>
+                        <ul className="header--signedout">
+                            <li><a href="sign-up.html">Sign Up</a></li>
+                            <li><a href="sign-in.html">Sign In</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </header>
-        </div>
+            <main>
+                <div className="wrap main--grid">
+                    <a className="course--module course--link" href="course-detail.html">
+                        <h2 className="course--label">Course</h2>
+                        <h3 className="course--title">Build a Basic Bookcase</h3>
+                    </a>
+                    <a className="course--module course--link" href="course-detail.html">
+                        <h2 className="course--label">Course</h2>
+                        <h3 className="course--title">Learn How to Program</h3>
+                    </a>
+                    <a className="course--module course--link" href="course-detail.html">
+                        <h2 className="course--label">Course</h2>
+                        <h3 className="course--title">Learn How to Test Programs</h3>
+                    </a>
+                    <a className="course--module course--add--module" href="create-course.html">
+                        <span className="course--add--title">
+                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                            viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+                            New Course
+                        </span>
+                    </a>
+                </div>
+            </main>
+        </React.Fragment>
+        
     );
 }
 
