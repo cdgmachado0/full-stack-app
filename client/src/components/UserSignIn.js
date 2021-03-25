13f1f56e456f8b528045781c1b4b12f206eda5f7
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-03-25 01:00:34 +0100

    Goog

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,15 +1,33 @@
 import React, {
     useContext,
-    useState
+    useState,
+    useRef
 } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../Context';
 
 
+
 function UserSignIn() {
     const { actions } = useContext(Context);
     const [ authenticated, setAuth ] = useState(false);
+    const emailAddress = useRef(); // emailAddress.current.value
+    const password = useRef();
 
+    const signIn = (e) => {
+        e.preventDefault();
+        //getting the authorization user to update authenticated state
+        console.log(emailAddress.current.value, password.current.value);
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+            }
+        };
+        return fetch('/users', options);
+    }
+    
     return (
         <React.Fragment>
             <header>
@@ -29,10 +47,10 @@ function UserSignIn() {
                 
                 <form>
                     <label htmlFor="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email"/>
+                    <input id="emailAddress" name="emailAddress" type="email" ref={emailAddress}/>
                     <label htmlFor="password">Password</label>
-                    <input id="password" name="password" type="password"/>
-                    <button className="button" type="submit">Sign In</button><button className="button button-secondary" onClick={(e) => actions.goBack(e, '/')}>Cancel</button>
+                    <input id="password" name="password" type="password" ref={password}/>
+                    <button className="button" type="submit" onClick={(e) => signIn(e)}>Sign In</button><button className="button button-secondary" onClick={(e) => actions.goBack(e, '/')}>Cancel</button>
                 </form>
                 <p>Don't have a user account? Click here to <a href="sign-up.html">sign up</a>!</p>
                 
